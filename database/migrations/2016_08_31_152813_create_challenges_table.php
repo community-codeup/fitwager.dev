d79e5e838a8326efaeff1ffb4a8f8f6af49e228b
TJ-Becker <TJ.Becker@mail.com>
2016-08-31 10:56:06 -0500

    Finished Database Migrations.  Made progress on seeders.

---
+++ database/migrations/2016_08_31_152813_create_challenges_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChallengesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('challenges', function (Blueprint $table){
+            $table->increments('id');
+            $table->mediumText('description');
+            $table->integer('created_by')->unsigned();
+            $table->integer('bet_type')->unsigned();
+            $table->integer('challenge_type')->unsigned();
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->timestamps();
+
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('bet_type')->references('id')->on('bet_types');
+            $table->foreign('challenge_type')->references('id')->on('challenge_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('Challenges');
+    }
+}

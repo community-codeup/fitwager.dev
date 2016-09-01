dc229bd030aebd846cfc0b50613346592c0f8717
Chris Davila <cmd1603@gmail.com>
2016-09-01 09:33:03 -0500

    Saving profile page/adding images and footer blade

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,28 @@
+<div id="footerFragment">
+    <footer id="glbfooter" role="contentInfo" class="gh-w">
+        <div class="container">
+            <div id="footer-table">
+                <table class="container-fluid text-center">
+                    <tbody>
+                    <tr>
+                        <td>
+                            <ul>
+                                <li><h5><a href="#" id="footer-heads">HOME</a></h5></li>
+                            </ul>
+                        </td>
+                        <td>
+                            <ul>
+                                <li><h5><a href="#" id="footer-heads">ACCOUNT</a></h5></li>
+                            </ul>
+                        </td>
+                        <td>
+                            <ul>
+                                <li><h5><a href="#" id="footer-heads">COINS</a></h5></li>
+                            </ul>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+    </footer>
+</div>

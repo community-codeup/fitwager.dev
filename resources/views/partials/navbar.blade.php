d665449f175bc7fab51d869a99a4c6369ffdf917
Chris Davila <cmd1603@gmail.com>
2016-09-04 12:27:33 -0500

    Saving navbar bug fix and challenges page addition/css changes

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,7 +3,7 @@
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-9" aria-expanded="false">
+                    data-target=".navbar-collapse" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -25,7 +25,7 @@
                                                                                         id="navButtons">|</span></li>
                     <li>
                         <a class="navbar-brand btn btn-lg btn-link" id="navButtons">
-                            <span class="glyphicon glyphicon-envelope"></span>
+                            <span id="envelope" class="glyphicon glyphicon-envelope"></span>
                             <span class="badge badge-notify">3</span>
                         </a>
                     </li>

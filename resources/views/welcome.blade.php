90b78ec5370dd10fafc0a28591f4e14433852fb9
Chris Davila <cmd1603@gmail.com>
2016-09-04 13:27:09 -0500

    Housecleaning

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,28 +59,28 @@
         {{-- END OF PAGINATION --}}
 
         {{-- START LEADERBOARD COLUMNS --}}
-    <div class="container">
+    <div class="container text-center">
         <div class="row">
             <div class="col-md-12"><h1 class="text-center">LEADERBOARD</h1></div>
         </div>
     </div>
 
-    <div class="container">
+    <div class="container text-center">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-4" id="leaderboardColumns">
                 <section id="content">
                     <h2>1st Content Area</h2>
                     <p>This page demonstrates a 3 column responsive layout, complete with responsive images and jquery slideshow.</p>
                 </section>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4" id="leaderboardColumns">
                 <section id="middle">
                     <h2>2nd Content Area</h2>
                     <p>At full width all three columns will be displayed side by side. As the page is resized the third column will collapse under the first and second. At the smallest screen size all three columns will be stacked on top of one another.</p>
                     <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
                 </section>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4" id="leaderboardColumns">
                 <aside id="sidebar">
                     <h2>3rd Content Area</h2>
                     <p>Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>

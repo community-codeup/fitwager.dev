6b78c075b2715d2a39b7d4f8e88c52d4a92ccbcf
Chris Davila <cmd1603@gmail.com>
2016-09-12 14:45:32 -0500

    Saving css changes to result blade

--- resources/views/challenges/result.blade.php
+++ resources/views/challenges/result.blade.php
@@ -6,31 +6,44 @@
 
     <div class="container-fluid" style="margin-top: 5%; padding-bottom: 2%;">
         <div class="row">
-            <div class="col-sm-3" style="padding-left: 3%">
-                <div style="border: 2px solid #00d053; border-radius: 2%; display: inline-block; padding-right: 3%; padding-left: 3%;">
-                    <div><label for="created_by" class="control-label"><h4 id="results_categories">created by:</span> {{$challenge->user->name}}</h4></label></div>
-                    <div><label for="created_by" class="control-label"><h4 id="results_categories">bet type: {{$challenge->betType->name}}</h4></label></div>
-                    <div><label for="created_by" class="control-label"><h4 id="results_categories">challenge type: {{$challenge->challengeType->name}}</h4></label></div>
-                    <div><label for="created_by" class="control-label"><h4 id="results_categories">wager: {{$challenge->wager}}</h4></label></div>
-                    <div><label for="created_by" class="control-label"><h4 id="results_categories">winners: </h4></label></div>
-                    <ul>
-                        @foreach ($winners as $winner)
-                            <li>{{$winner->user->name}} {{$winner->winnings}}</li>
-                        @endforeach
-                    </ul>
-                    <div><label for="created_by" class="control-label"><h4>losers: </h4></label></div>
-                    <ul>
-                        @foreach ($losers as $loser)
-                            <li>{{$loser->user->name}} {{$loser->winnings}}</li>
-                        @endforeach
-                    </ul>
+            <div class="col-md-12 text-center" style="padding-left: 3%">
+                <div style="border: 2px solid #00d053; border-radius: 2%;">
+                    <label for="created_by" class="control-label results"><h3>created by: </h3><h4>{{$challenge->user->name}}</h4></label>
+                    <label for="bet_type" class="control-label results"><h3>bet type: </h3><h4>{{$challenge->betType->name}}</h4></label>
+                    <label for="challenge_type" class="control-label results"><h3>challenge type: </h3><h4>{{$challenge->challengeType->name}}</h4></label>
+                    <label for="wager" class="control-label results"><h3>wager: </h3><h4>{{$challenge->wager}}</h4></label>
                 </div>
             </div>
+        </div>
 
+    <br>
+
+        <div class="row">
                 <script>
                     var graphInfo = {!! json_encode($graphInfo) !!};
                 </script>
-                <div class="col-sm-9" id="resultChart" style="min-width: 200px; height: 400px; margin: 0 auto"></div>
+                <div class="col-md-12" id="resultChart" style="min-width: 200px; height: 400px; margin: 0 auto"></div>
+        </div>
+
+    </div>
+
+    <div class="row">
+        <div class="col-md-6 text-center">okoodfdf
+            <label for="created_by" class="control-label"><h4 id="results_categories">winners: </h4></label>
+            <ul>
+                @foreach ($winners as $winner)
+                    <li>{{$winner->user->name}} {{$winner->winnings}}</li>
+                @endforeach
+            </ul>
+        </div>
+
+        <div class="col-md-6 text-center">
+            <label for="created_by" class="control-label"><h4>losers: </h4></label>
+            <ul>
+                @foreach ($losers as $loser)
+                    <li>{{$loser->user->name}} {{$loser->winnings}}</li>
+                @endforeach
+            </ul>
         </div>
     </div>
 

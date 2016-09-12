fce8f195c5689ee402153c6d3560036d1aabf446
TJ-Becker <TJ.Becker@mail.com>
2016-09-12 14:36:23 -0500

    fixed middleware to prevent non logged in users from reaching pages.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -23,7 +23,7 @@ class UsersController extends Controller
 {
     public function __construct()
     {
-       // $this->middleware('auth');
+       $this->middleware('auth');
     }
     /**
      * Display a listing of the resource.

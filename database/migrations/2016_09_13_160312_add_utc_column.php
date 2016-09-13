e34b165f107a362b67dfddc4481e059631fa6204
TJ-Becker <TJ.Becker@mail.com>
2016-09-13 11:13:55 -0500

    added column to users table to take care of utc offset

---
+++ database/migrations/2016_09_13_160312_add_utc_column.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUtcColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('utc_offset')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('utc_offset');
+        });
+    }
+}
